caf8608e006da67e1a47cc8cdc5135535dac4bce
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2022-11-14 15:06:50 -0500

    rename file to convey meaning

--- playground.py
+++ playground.py
@@ -1,5 +1,9 @@
 from preprocessing.ridership_data import RidershipData, RawRidershipData
 
+"""
+This script is purely for informal testing and 'playing' with new code. 
+"""
+
 RRD = RawRidershipData('data/ridership_data/SFMTA.xlsx', 'SF')
 RD = RidershipData(RRD)
 print(RD.read_data())
