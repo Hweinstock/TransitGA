2a2b91b6f4f156983d5dccb16c3705b21d86ee39
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2022-11-14 14:54:21 -0500

    start skeleton classes for data/Users/hweinstock/Documents/GitHub/Math-thesis/venv/bin/python

--- preprocessing/ridership_data.py
+++ preprocessing/ridership_data.py
@@ -1,12 +1,26 @@
 import pandas as pd  
-import pathlib 
+from preprocessing.data import DataBase
 
-class RidershipData():
+class RawRidershipData(DataBase):
 
-    def read_data(self):
+    def read_data(self) -> pd.DataFrame:
         df = pd.read_excel(self.filepath)
         return df 
 
+class RidershipData(DataBase):
+
+    def __init__(self, raw_data : RawRidershipData):
+        self.raw_data = raw_data
+        DataBase.__init__(raw_data.filepath, raw_data.city_name)
+
+    def read_data(self):
+        raw_df = self.raw_data.read_data()
+        
+        return raw_df
+    
+    
+
 if __name__ == '__main__':
-    RD = RidershipData('data/ridership_data/SFMTA.xlsx')
+    RRD = RawRidershipData('data/ridership_data/SFMTA.xlsx')
+    RD = RidershipData(RRD)
     print(RD.read_data())
