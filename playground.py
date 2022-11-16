33a4d8942fae7126690c9482c8fe0717b65bc4c5
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2022-11-16 10:28:45 -0500

    start gtfs parsing

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
 from preprocessing.ridership_data import RidershipData, RawRidershipData
-
+from preprocessing.gtfs_data import GTFSData
 """
 This script is purely for informal testing and 'playing' with new code. 
 """
@@ -7,3 +7,6 @@ This script is purely for informal testing and 'playing' with new code.
 RRD = RawRidershipData('data/ridership_data/SFMTA.xlsx', 'SF')
 RD = RidershipData(RRD)
 RD.export_data()
+
+SF_GTFS = GTFSData('data/gtfs_data/SFMTA.zip', 'SF')
+print(type(SF_GTFS.read_data()))
--- preprocessing/gtfs_data.py
+++ preprocessing/gtfs_data.py
@@ -1,5 +1,16 @@
 from preprocessing.data import DataBase
+import gtfs_kit as gk
+from pathlib import Path
+
 
 class GTFSData(DataBase):
 
-    pass
+    def __init__(self, filepath, city_name):
+        DataBase.__init__(self, filepath, city_name)
+        path = Path(filepath)
+        self.feed = gk.read_feed(path, dist_units='mi')
+
+    def read_data(self):
+        return self.feed
+    
+    
