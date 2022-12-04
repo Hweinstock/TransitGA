865a636afd8880d064e6c384a851820f1c2dd475
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2022-12-04 14:13:37 -0500

    switched to colorama

--- root_logger.py
+++ root_logger.py
@@ -1,4 +1,5 @@
 import logging
+from colorama import Fore, Back, Style
 
 class bcolors:
     HEADER = '\033[95m'
@@ -12,13 +13,16 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class RootLogger:
-    LEVEL = logging.ERROR
+    LEVEL = logging.INFO
 
     def log_info(msg):
-        logging.info(f'{bcolors.OKBLUE}{msg}{bcolors.ENDC}')
+        logging.info(f'{Fore.BLUE}{msg}{Style.RESET_ALL}')
     
     def log_warning(msg):
-        logging.warning(f'{bcolors.WARNING}{msg}{bcolors.ENDC}')
+        logging.warning(f'{Fore.YELLOW}{msg}{Style.RESET_ALL}')
     
     def log_debug(msg):
-        logging.debug(f'{bcolors.OKGREEN}{msg}{bcolors.ENDC}')
+        logging.debug(f'{Fore.GREEN}{msg}{Style.RESET_ALL}')
+    
+    def log_error(msg):
+        logging.error(f'{Fore.RED}{msg}{Style.RESET_ALL}')
